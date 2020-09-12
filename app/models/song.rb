6e957446d54d07b1d7129882e59b501b847837e7
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-09-12 15:32:48 +0900

    activestorageの導入

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,5 @@ class Song < ApplicationRecord
   validates :name, :text, :image, :genre_id, presence: true
   validates :genre_id, numericality: { other_than: 1 } 
   belongs_to :user
+  has_one_attached :image
 end
