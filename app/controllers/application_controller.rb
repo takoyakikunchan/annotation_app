225812574f7ae04e939ac98fe0cb1fa4e1d95032
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-09-24 23:01:05 +0900

    userモデルの結合コード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  before_action :basic_auth,if: :production?
 
   private
+  
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
